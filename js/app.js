a3e699af4b43b984fb5eed80bf6f3f7604ad3482
otu222 <142851068+otu222@users.noreply.github.com>
2024-02-29 17:26:32 -0500

    Improve JSON reading and Promises, Update Selector

--- js/app.js
+++ js/app.js
@@ -27,79 +27,40 @@ function getCensus() {
     // need to do this with the rest of the datas (total 10 data)
 
     // new function
-    a.allCensusAPIs.population25 = fetch(a.censusURLs.population25).then(function (r) {
-        return r.json();
-    });
-    a.allCensusAPIs.gradeLessThan9th = fetch(a.censusURLs.gradeLessThan9th).then(function (r) {
-        return r.json();
-    });
-    a.allCensusAPIs.grade9thto12th = fetch(a.censusURLs.grade9thto12th).then(function (r) {
-        return r.json();
-    });
-    a.allCensusAPIs.gradeHighSchoolGrad = fetch(a.censusURLs.gradeHighSchoolGrad).then(function (r) {
-        return r.json();
-    });
-    a.allCensusAPIs.gradeSomeCollegeNoDegree = fetch(a.censusURLs.gradeSomeCollegeNoDegree).then(function (r) {
-        return r.json();
-    });
-    a.allCensusAPIs.gradeAssociates = fetch(a.censusURLs.gradeAssociates).then(function (r) {
-        return r.json();
-    });
-    a.allCensusAPIs.gradeBachelors = fetch(a.censusURLs.gradeBachelors).then(function (r) {
-        return r.json();
-    });
-    a.allCensusAPIs.gradeGradOrPro = fetch(a.censusURLs.gradeGradOrPro).then(function (r) {
-        return r.json();
-    });
-    a.allCensusAPIs.certHighSchoolGrad = fetch(a.censusURLs.certHighSchoolGrad).then(function (r) {
-        return r.json();
-    });
-    a.allCensusAPIs.certBachelors = fetch(a.censusURLs.certBachelors).then(function (r) {
-        return r.json();
-    });
+
+    let censusPromises = [];
+
+    for (let dataSet in a.censusData) {
+        a.variables.push(dataSet);
+        a.censusData[dataSet].censusResponse = fetch(a.censusData[dataSet].apiURL).then(function (r) {
+            return r.json();
+        });
+        censusPromises.push(a.censusData[dataSet].censusResponse);
+    }
     a.geometry = fetch(a.geojsons.counties).then(function (r) {
         return r.json();
     });
-
-    Promise.all([
-        a.allCensusAPIs.population25,
-        a.allCensusAPIs.gradeLessThan9th,
-        a.allCensusAPIs.grade9thto12th,
-        a.allCensusAPIs.gradeHighSchoolGrad,
-        a.allCensusAPIs.gradeSomeCollegeNoDegree,
-        a.allCensusAPIs.gradeAssociates,
-        a.allCensusAPIs.gradeBachelors,
-        a.allCensusAPIs.gradeGradOrPro,
-        a.allCensusAPIs.certHighSchoolGrad,
-        a.allCensusAPIs.certBachelors,
-        a.geometry
-    ])
+    censusPromises.push(a.geometry);
+    
+    Promise.all(censusPromises)
         .then(function (response) {
-            a.data.pop25 = response[0];
-            a.data.lessThan9th = response[1];
-            a.data.grade9to12 = response[2];
-            a.data.gradeHSGrad = response[3];
-            a.data.gradeSomeCollege = response[4];
-            a.data.gradeAssociates = response[5];
-            a.data.gradeBachelors = response[6];
-            a.data.gradeGradOrPro = response[7];
-            a.data.certHSGrad = response[8];
-            a.data.certBachelors = response[9];
-            a.data.geometryData = response[10];
-            console.log(a.data)
+            for (let i = 0; i < a.variables.length; i++) {
+                a.censusData[a.variables[i]].jsonData = response[i];
+            }
+            a.geometryData = response[10];
 
-            for (let n of a.data.pop25) {
+            for (let n of a.censusData["pop25"].jsonData) {
                 const countyFips = n[2] + n[3];
-                for (const m of a.data.geometryData.features) {
-                const geoid = m.properties.GEOID;
-                if (geoid == countyFips) {
-                    // if (n[0] == "Fayette County, Kentucky") {
-                    // console.log(n);
-                    // }
-                    m.properties.census = {};
-                    m.properties.census.home = n[1];
-                    break;
-                }
+                for (const m of a.geometryData.features) {
+                    const geoid = m.properties.GEOID;
+                    if (geoid == countyFips) {
+                        if (n[0] == "Fayette County, Kentucky") {
+                            console.log(n);
+                        }
+                        m.properties.census = {};
+                        m.properties.census.home = n[1];
+                        break;
+                    }
                 }
             }
             drawMap();
@@ -107,41 +68,10 @@ function getCensus() {
         .catch(function (error) {
             console.log(error);
         });
-
-    // old function as template
-    // a.censusAPI = fetch(a.censusURLs.certBachelors).then(function (r) {
-    //     return r.json();
-    // });
-    // a.geometry = fetch(a.geojsons.counties).then(function (r) {
-    //     return r.json();
-    // });
-    // Promise.all([a.censusAPI, a.geometry])
-    //     .then(function (response) {
-    //         a.data.censusData = response[0];
-    //         a.data.geometryData = response[1];
-    //         for (let n of a.data.censusData) {
-    //             const countyFips = n[2] + n[3];
-    //             for (const m of a.data.geometryData.features) {
-    //             const geoid = m.properties.GEOID;
-    //             if (geoid == countyFips) {
-    //                 // if (n[0] == "Fayette County, Kentucky") {
-    //                 // console.log(n);
-    //                 // }
-    //                 m.properties.census = {};
-    //                 m.properties.census.home = n[1];
-    //                 break;
-    //             }
-    //             }
-    //         }
-    //         drawMap();
-    //     })
-    //     .catch(function (error) {
-    //         console.log(error);
-    //     });
 }
 
 function drawMap() {
-    a.layers.placed = L.geoJson(a.data.geometryData, {
+    a.layers.placed = L.geoJson(a.geometryData, {
         style: function (feature) {
         return a.map.styles.default;
         },
@@ -154,9 +84,9 @@ function drawMap() {
         });
         },
         filter: function (feature) {
-        for (let i of a.data.censusData) {
+        for (let i of a.censusData["pop25"].jsonData) {
             if (feature.properties.STATEFP == i[2]) {
-            return true;
+                return true;
             }
         }
         },
@@ -226,7 +156,6 @@ function addLegend() {
 
 function updateLegend() {
     const breaks = a.classes.breaks;
-    // console.log(a.censusVar)
     legend.innerHTML = `<h5>${a.censusVar}</h5>`;
 
     for (let i = 0; i <= breaks.length - 1; i++) {
@@ -241,18 +170,17 @@ function updateLegend() {
 }
 
 function addUi() {
-    console.log("Stuff")
     a.buttons.dropdown.placed = document.querySelector(
         a.buttons.dropdown.select
     );
     a.buttons.dropdown.placed.innerHTML = "";
     for (let i = 1; i < a.variables.length; i++) {
         let option = document.createElement("option");
-        option.textContent = a.variables[i].label;
-        option.value = a.variables[i].name;
-        if (a.variables[i].name == a.vars.aV) {
-        option.selected = true;
-        }
+        option.textContent = a.censusData[a.variables[i]].legendLabel;
+        option.value = a.variables[i];
+        // if (a.variables[i].name == a.vars.aV) {
+        //     option.selected = true;
+        // }
         a.buttons.dropdown.placed.appendChild(option);
     }
     const selectControl = L.control(a.buttons.dropdown.options);
@@ -261,7 +189,7 @@ function addUi() {
     };
     selectControl.addTo(a.map.placed);
     a.buttons.dropdown.placed.addEventListener("change", function (e) {
-        a.vars.aV = e.target.value;
+        // a.vars.aV = e.target.value;
         updateMap();
     });
 }
